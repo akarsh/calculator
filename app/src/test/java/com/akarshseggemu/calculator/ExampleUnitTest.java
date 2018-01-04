7d90c3d6713f8e3875104f13fc83bb01a39a3ba2
akarsh <sakarshkumar@gmail.com>
2018-01-04 21:34:24 +0100

    Update unit tests

--- app/src/test/java/com/akarshseggemu/calculator/ExampleUnitTest.java
+++ app/src/test/java/com/akarshseggemu/calculator/ExampleUnitTest.java
@@ -1,17 +1,21 @@
 package com.akarshseggemu.calculator;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
+//    private Calculator mCalculator;
+//
+//    @Before
+//    public void setUP(){
+//        mCalculator = new Calculator();
+//    }
+//
+//    @Test
+//    public void calculator_CorrectAdd_ReturnsTrue(){
+//        double resultAdd = mCalculator.add(3,4);
+//        assertEquals("adding 3+ 4 didn' work this time", 7, resultAdd, 0);
+//    }
 }
